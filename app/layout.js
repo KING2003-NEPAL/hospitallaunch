44aa674074d9dad5df5ce02f096daba488513744
KING2003-NEPAL <deepakshrestha1166@gmail.com>
2026-03-10 13:01:33 +0000

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-export const metadata = { title: "HospitaLaunch", description: "Career Passport Platform" };
+export const metadata = { title: "HospitaLaunch" };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
